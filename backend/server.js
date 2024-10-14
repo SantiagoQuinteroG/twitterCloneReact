1ebad33d418fbe08de7dba70f994d3698b731822
Santiago Quintero Gonzalez <>
2024-10-13 22:33:20 -0500

    User routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+pp.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
